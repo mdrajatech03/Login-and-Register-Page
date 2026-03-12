56d93c1ac2603b41a703adb1f83cc3214145c078
Md Raja <mdraja.tech.03@gmail.com>
2026-03-13 01:12:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,34 +4,15 @@ const registerLink = document.querySelector('.login-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
-// Switch to Register Form
-loginLink.onclick = (e) => {
-    e.preventDefault();
-    wrapper.classList.add('active');
-};
+loginLink.onclick = () => wrapper.classList.add('active');
+registerLink.onclick = () => wrapper.classList.remove('active');
+btnPopup.onclick = () => wrapper.classList.add('active-popup');
+iconClose.onclick = () => wrapper.classList.remove('active-popup');
 
-// Switch to Login Form
-registerLink.onclick = (e) => {
-    e.preventDefault();
-    wrapper.classList.remove('active');
-};
-
-// Open Popup
-btnPopup.onclick = () => {
-    wrapper.classList.add('active-popup');
-};
-
-// Close Popup
-iconClose.onclick = () => {
-    wrapper.classList.remove('active-popup');
-};
-
-// Password Show/Hide Toggle
+// Password Toggle
 document.querySelectorAll('.toggle-password').forEach(icon => {
     icon.onclick = function() {
-        const inputId = this.getAttribute('data-target');
-        const input = document.getElementById(inputId);
-        
+        const input = document.getElementById(this.getAttribute('data-target'));
         if (input.type === 'password') {
             input.type = 'text';
             this.classList.replace('fa-eye-slash', 'fa-eye');
